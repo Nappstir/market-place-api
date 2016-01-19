6588c8ab3dd993d13c38a19dcf1f823638dc17e5
Travis Siebenhaar <trsiebenhaar@gmail.com>
2016-01-19 14:12:04 -0700

    adjusted rspec file

--- lib/spec/api_constraints_spec.rb
+++ lib/spec/api_constraints_spec.rb
@@ -5,7 +5,6 @@ describe ApiConstraints do
   let(:api_constraints_v2) { ApiConstraints.new(version: 2, default: true) }
 
   describe "matches?" do
-    
     it "returns true when the version matches the 'Accept' header" do
       request = double(host: 'api.marketplace.dev', headers: {"Accept" => "application/vnd.marketplace.v1"})
       api_constraints_v1.matches?(request).should be_true
@@ -16,4 +15,5 @@ describe ApiConstraints do
       api_constraints_v2.matches?(request).should be_true
     end
   end
+
 end
