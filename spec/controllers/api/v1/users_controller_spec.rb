0367241aa1c48f4f79472e2e590aa79f56e534a6
Travis Siebenhaar <trsiebenhaar@gmail.com>
2016-01-19 16:32:19 -0700

    Add JSON responses to spec test.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
+  before(:each) { request.headers["Accept"] = "application/vnd.marketplace.v1" }
+
   describe "GET #show" do
-    before(:each) do 
-      @user = FactoryGirl.create :user
-      get :show, id: @user.id
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      get :show, id: @user.id, format: :json
     end
 
-    it "returns the information about a reporter on a hash" do
-      user_response = json_response
+    it "returns the information about a user on a hash" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
 
@@ -17,33 +19,33 @@ describe Api::V1::UsersController do
 
   describe "POST #create" do
 
-    context "when is successfully created" do
+    context "when user is successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
         post :create, { user: @user_attributes }
       end
 
       it "renders the json representation for the user record just created" do
-        user_response = json_response
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:email]).to eql @user_attributes[:email]
       end
 
       it { should respond_with 201 }
     end
 
-    context "when is not created" do
+    context "when user is not created" do
       before(:each) do
-        @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" } #notice I'm not including the email
+        @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" }
         post :create, { user: @invalid_user_attributes }
       end
 
-      it "renders an errors json" do
-        user_response = json_response
+      it "renders an error" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on whye the user could not be created" do
-        user_response = json_response
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
 
@@ -53,34 +55,34 @@ describe Api::V1::UsersController do
 
   describe "PUT/PATCH #update" do
     before(:each) do
-      @user = FactoryGirl.create :user
+      @user = FactoryGirl.create(:user)
     end
 
-    context "when is successfully updated" do
+    context "when a user is successfully updated" do
       before(:each) do
         patch :update, { id: @user.id, user: { email: "newmail@example.com" } }
       end
 
       it "renders the json representation for the updated user" do
-        user_response = json_response
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:email]).to eql "newmail@example.com"
       end
 
       it { should respond_with 200 }
     end
 
-    context "when is not created" do
+    context "when a user is not created" do
       before(:each) do
         patch :update, { id: @user.id, user: { email: "bademail.com" } }
       end
 
-      it "renders an errors json" do
-        user_response = json_response
+      it "renders errors" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on whye the user could not be created" do
-        user_response = json_response
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "is invalid"
       end
 
