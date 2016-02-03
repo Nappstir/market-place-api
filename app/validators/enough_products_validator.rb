4fdeb00211641a1550f98a6040a7f98ca7c05fa7
Travis Siebenhaar <trsiebenhaar@gmail.com>
2016-02-02 18:35:29 -0700

    Changes record -> order for 'enough_products_validator'

--- app/validators/enough_products_validator.rb
+++ app/validators/enough_products_validator.rb
@@ -1,10 +1,10 @@
 class EnoughProductsValidator < ActiveModel::Validator
 
-  def validate(record)
-    record.placements.each do |placement|
+  def validate(order)
+    order.placements.each do |placement|
       product = placement.product
       if placement.quantity > product.quantity
-        record.errors["#{product.title}"] << "is out of stock, just #{product.quantity} left"
+        order.errors["#{product.title}"] << "is out of stock, just #{product.quantity} left"
       end
     end
   end
